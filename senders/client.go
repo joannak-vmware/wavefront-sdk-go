7d0bc38cf96d60552c4369b622ca29d27f3f8f22
Joanna Ko <joannak@vmware.com>
2020-10-14 22:59:02 -0700

    Fixed error

--- senders/client.go
+++ senders/client.go
@@ -97,7 +97,7 @@ func (sender *wavefrontSender) SendMetric(name string, value float64, ts int64, 
 	return sender.pointHandler.HandleLine(line)
 }
 
-func (sender *wavefrontSender) sendDeltaCounter(name string, value float64, ts int64, source string, tags map[string]string) error {
+func (sender *wavefrontSender) SendDeltaCounter(name string, value float64, ts int64, source string, tags map[string]string) error {
 	if name == "" {
 		return fmt.Errorf("empty metric name")
 	}
